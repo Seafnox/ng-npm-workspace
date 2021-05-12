b967c86a8f452e710af21fbf54f488867b9d13b2
Kirill Warp <kirill9990@gmail.com>
2021-05-12 12:15:33 +0300

    add auth from schematics without changes

---
+++ libs/auth/src/lib/auth.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { AuthComponent } from 'libs/auth/src/lib/auth.component';
+
+
+
+@NgModule({
+  declarations: [
+    AuthComponent
+  ],
+  imports: [
+  ],
+  exports: [
+    AuthComponent
+  ]
+})
+export class AuthModule { }
---
+++ libs/auth/src/libs.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of auth
+ */
+
+export * from 'libs/auth/src/lib/auth.service';
+export * from 'libs/auth/src/lib/auth.component';
+export * from 'libs/auth/src/lib/auth.module';
