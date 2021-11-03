302c361408672604c4a9ba9cb740e1f8cf556e5a
Kirill Warp <kirill9990@gmail.com>
2021-11-03 21:17:53 +0300

    start auth page work

--- libs/auth/src/lib/auth.module.ts
+++ libs/auth/src/lib/auth.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { AuthComponent } from 'libs/auth/src/lib/auth.component';
+import { AuthFormComponent } from './auth-form/auth-form.component';
 
 @NgModule({
   declarations: [
     AuthComponent,
+    AuthFormComponent,
   ],
   imports: [],
   exports: [
