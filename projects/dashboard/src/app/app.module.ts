7f576549fa726ffd02520e1b151c23f3194b9b63
Kirill Warp <kirill9990@gmail.com>
2021-11-03 21:17:17 +0300

    add ng-zorro design system

--- projects/dashboard/src/app/app.module.ts
+++ projects/dashboard/src/app/app.module.ts
@@ -3,6 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TableModule } from '@ng-npm7-example/table';
+import { NZ_I18N, ru_RU } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import ru from '@angular/common/locales/ru';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { IconsProviderModule } from './icons-provider.module';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
+
+registerLocaleData(ru);
 
 @NgModule({
   declarations: [
@@ -12,8 +23,14 @@ import { TableModule } from '@ng-npm7-example/table';
     BrowserModule,
     AppRoutingModule,
     TableModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    IconsProviderModule,
+    NzLayoutModule,
+    NzMenuModule,
   ],
-  providers: [],
+  providers: [{provide: NZ_I18N, useValue: ru_RU}],
   bootstrap: [AppComponent],
 })
 export class AppModule {
